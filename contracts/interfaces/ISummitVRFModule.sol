0ab7a93ab7e704b7d5456ba7e9df4490a045ff2f
daiki saito <daiki.saito92@gmail.com>
2021-12-02 00:43:15 +0900

    remove unnecessary functions

--- contracts/interfaces/ISummitVRFModule.sol
+++ contracts/interfaces/ISummitVRFModule.sol
@@ -3,14 +3,7 @@ pragma solidity 0.8.0;
 
 
 interface ISummitVRFModule {
-    function futureBlockMined() external view returns (bool);
-    function nextSeedRoundAvailable() external view returns (bool);
     function getRandomNumber(uint256 roundNumber) external view returns (uint256);
-
-    function setElevationHelper (address _elevationHelper) external;
     function setTrustedSeederAdd(address _trustedSeeder) external;
-    function setSeedRoundEndTimestamp(uint256 _seedRoundEndTimestamp) external;  
-
-    function receiveSealedSeed(bytes32 _sealedSeed) external;
-    function receiveUnsealedSeed(bytes32 _unsealedSeed) external;
+    function setSeedRoundEndTimestamp(uint256 _seedRoundEndTimestamp) external;
 }
